18534200e896d6df56aedb98b596100d548af071
Julian König <julian.koenig@js-soft.com>
2020-02-12 16:54:38 +0100

    fix: Url encoded Arguments

--- src/main/kotlin/com/github/seliba/devcordbot/commands/general/LmgtfyCommand.kt
+++ src/main/kotlin/com/github/seliba/devcordbot/commands/general/LmgtfyCommand.kt
@@ -22,6 +22,7 @@ import com.github.seliba.devcordbot.command.context.Arguments
 import com.github.seliba.devcordbot.command.context.Context
 import com.github.seliba.devcordbot.command.perrmission.Permission
 import com.github.seliba.devcordbot.constants.Embeds
+import java.net.URLEncoder
 
 /**
  * Lmgtfy command.
@@ -47,5 +48,8 @@ class LmgtfyCommand : AbstractCommand() {
     /**
      * Create a lmgtfy link.
      */
-    private fun createLink(text: Arguments) = "https://lmgtfy.com/?q=${text.joinToString("+")}&iie=1"
+    private fun createLink(arguments: Arguments) =
+        "https://lmgtfy.com/?q=${arguments.joinToString("+") {
+            URLEncoder.encode(it, "utf-8")
+        }}&iie=1"
 }
