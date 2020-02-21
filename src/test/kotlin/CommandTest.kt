a300eb3687f8e2489d5efe71d6c110b63047ae72
das_ <git@heap.mozmail.com>
2020-02-21 22:22:40 +0100

    Fix CommandTest.kt fail

--- src/test/kotlin/CommandTest.kt
+++ src/test/kotlin/CommandTest.kt
@@ -36,22 +36,6 @@ import java.util.concurrent.CompletableFuture
 import java.util.function.BooleanSupplier
 import java.util.function.Consumer
 
-/*
- * Copyright 2020 Daniel Scherf & Michael Rittmeister
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 class CommandTest {
 
     @Test
@@ -93,7 +77,7 @@ class CommandTest {
 
         val command = mockCommand {
             on { aliases }.thenReturn(listOf("test"))
-            on { commandAssociations }.thenReturn(mutableMapOf("sub" to subCommand))
+            on { subCommandAssociations }.thenReturn(mutableMapOf("sub" to subCommand))
         }
 
         ensureCommandCall(message, command, arguments.subList(1, arguments.size), subCommand)
