9bae1821b5ab5fd76f27054ffc99ca6c6d942c08
Julian König <julian.koenig@js-soft.com>
2020-06-15 22:25:10 +0200

    chore: faster splitting

--- src/main/kotlin/com/github/seliba/devcordbot/commands/general/TagCommand.kt
+++ src/main/kotlin/com/github/seliba/devcordbot/commands/general/TagCommand.kt
@@ -411,7 +411,7 @@ class TagCommand : AbstractCommand() {
     }
 
     private fun checkReservedName(name: String, context: Context): Boolean {
-        if (name.split(" ").first() in reservedNames) {
+        if (name.substring(0, name.indexOf(' ')) in reservedNames) {
             context.respond(
                 Embeds.error(
                     "Reservierter Name!",
