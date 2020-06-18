02b6d3c49fc9b10a8be5ef2cd6bfb7b1f5cedfd9
Julian König <33655937+jkoenig134@users.noreply.github.com>
2020-06-18 21:06:44 +0200

    feat: rename package from seliba to devcordde (#54)

---
+++ src/main/kotlin/com/github/devcordde/devcordbot/commands/general/InfoCommand.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Daniel Scherf & Michael Rittmeister & Julian König
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.github.devcordde.devcordbot.commands.general
+
+import com.github.devcordde.devcordbot.command.AbstractCommand
+import com.github.devcordde.devcordbot.command.CommandCategory
+import com.github.devcordde.devcordbot.command.CommandPlace
+import com.github.devcordde.devcordbot.command.context.Context
+import com.github.devcordde.devcordbot.command.permission.Permission
+import com.github.devcordde.devcordbot.listeners.SelfMentionListener
+
+/**
+ * InfoCommand
+ */
+class InfoCommand : AbstractCommand() {
+    override val aliases: List<String> = listOf("info")
+    override val displayName: String = "info"
+    override val description: String = "Zeigt Bot-Informationen an."
+    override val usage: String = ""
+    override val permission: Permission = Permission.ANY
+    override val category: CommandCategory = CommandCategory.GENERAL
+    override val commandPlace: CommandPlace = CommandPlace.ALL
+
+    override suspend fun execute(context: Context) {
+        SelfMentionListener.sendInfo(context.channel, context.jda.users.size)
+    }
+}
