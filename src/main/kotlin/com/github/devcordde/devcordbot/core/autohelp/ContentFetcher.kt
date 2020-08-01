4b449ff994e4b763d967b177d2c42d4ee25c4997
Michael Rittmeister <michael@rittmeister.in>
2020-08-01 15:16:22 +0200

    feature/autohelp overhaul (#97)
    * it's not as good as OCR but still good
    
    * Massive Autohelp overhaul & refactoring
    - Switch to openjdk javadocs because oracle is dumb
    - analyze code for npes
    - add too long errors again
    - improve handling of text message content for autohelp
    - code optimizations
    - fix crash when docs fetching fails
    
    * Massive Autohelp overhaul & refactoring
    - Add OCR again
    - Fix javadoc methods on JDK 10 (and probabbly others but i am done with HTML for now)
    
    * Resolve conflicts
    
    * Apply requested changes
    
    * Update _JavadocCommands.kt
    
    * Make people who like spelling happy

--- src/main/kotlin/com/github/devcordde/devcordbot/core/autohelp/ContentFetcher.kt
+++ src/main/kotlin/com/github/devcordde/devcordbot/core/autohelp/ContentFetcher.kt
@@ -16,20 +16,126 @@
 
 package com.github.devcordde.devcordbot.core.autohelp
 
+import com.github.devcordde.devcordbot.util.GithubUtil
 import com.github.devcordde.devcordbot.util.executeAsync
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.future.future
+import net.dv8tion.jda.api.entities.Message
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.concurrent.CompletableFuture
 
 /**
  * ContentFetcher
  */
-class ContentFetcher(private val httpClient: OkHttpClient) {
+class ContentFetcher(
+    private val httpClient: OkHttpClient,
+    private val github: GithubUtil,
+    private val executor: CoroutineDispatcher
+) {
+
+
+    /**
+     * Trigger AutoHelp on Message.
+     */
+    suspend fun fetchMessageContents(message: Message): List<CompletableFuture<out List<String?>>> {
+        val input = message.contentRaw
+
+        // Asynchronously fetch potential content
+        val attachments = fetchAttachments(message)
+        val messageContents: MutableList<CompletableFuture<List<String?>>> = if (input.isNotBlank()) {
+            val hastebinMatches = findInput(HASTEBIN_PATTERN, input, ::fetchHastebin)
+            val pastebinMatches = findInput(PASTEBIN_PATTERN, input, ::fetchPastebin)
+            val ghostbinMatches = findInput(GHOSTBIN_PATTERN, input, ::fetchGhostbin)
+            val githubMatches = findInputs(GITHUB_GIST_PATTERN, input, ::fetchGithub)
+
+            mutableListOf(hastebinMatches, pastebinMatches, ghostbinMatches, githubMatches)
+        } else mutableListOf()
+        return messageContents.plus(attachments).toList()
+    }
+
+    private fun findInput(
+        pattern: Regex,
+        input: String,
+        fetcher: (MatchResult) -> CompletableFuture<String?>
+    ): CompletableFuture<List<String?>> {
+        return GlobalScope.future(executor) {
+            pattern.findAll(input).toList().map {
+                fetcher(it).await()
+            }
+        }
+    }
+
+    private fun findInputs(
+        pattern: Regex,
+        input: String,
+        fetcher: (MatchResult, String) -> CompletableFuture<List<String?>>
+    ): CompletableFuture<List<String?>> {
+        return GlobalScope.future(executor) {
+            pattern.findAll(input).toList().map {
+                fetcher(it, input).await()
+            }.flatten()
+        }
+    }
+
+    private suspend fun fetchAttachments(message: Message): CompletableFuture<List<String?>> {
+        return GlobalScope.future(executor) {
+            message.attachments.filter { !it.isVideo }.map {
+                fetchAttachment(it)
+            }
+        }
+    }
+
+    private suspend fun fetchAttachment(attachment: Message.Attachment): String {
+        val stream = attachment.retrieveInputStream().await()
+        return if (attachment.isImage) {
+            ImageReader.readImage(stream) ?: ""
+        } else {
+            BufferedReader(InputStreamReader(stream)).use { reader ->
+                reader.readText()
+            }
+        }
+    }
+
+
+    private fun fetchHastebin(match: MatchResult): CompletableFuture<String?> {
+        val domain = match.groupValues[1]
+        val pasteId = match.groupValues[2]
+        val rawPaste = "https://$domain/raw/$pasteId"
+        return fetchWebContent(rawPaste)
+    }
+
+    private fun fetchPastebin(match: MatchResult): CompletableFuture<String?> {
+        val pasteId = match.groupValues[1]
+        val rawPaste = "https://pastebin.com/raw/$pasteId"
+        return fetchWebContent(rawPaste)
+    }
+
+    private fun fetchGhostbin(match: MatchResult): CompletableFuture<String?> {
+        val pasteId = match.groupValues[1]
+        val rawPaste = "https://ghostbin.co/paste/$pasteId/raw"
+        return fetchWebContent(rawPaste)
+    }
+
+    private fun fetchGithub(match: MatchResult, url: String): CompletableFuture<List<String?>> {
+        val host = match.groupValues[1]
+        val gistId = match.groupValues[3]
+        if (host == "gist.githubusercontent.com") {
+            return fetchWebContent(url).thenApply { listOf(it) }
+        }
+        return github.retrieveGistFiles(gistId).thenApplyAsync { fileUrls ->
+            fileUrls.map { fetchWebContent(it).join() }
+        }
+    }
 
     /**
      * Fetch content from given Url.
      */
-    fun fetchContent(url: String): CompletableFuture<String?> {
+    private fun fetchWebContent(url: String): CompletableFuture<String?> {
         val request = Request.Builder()
             .url(url)
             .get()
@@ -39,5 +145,25 @@ class ContentFetcher(private val httpClient: OkHttpClient) {
                 it?.string()
             }
         }
+    }
+
+    companion object {
+
+        // https://regex101.com/r/u0QAR6/7
+        private val HASTEBIN_PATTERN =
+            "(?:https?:\\/\\/)?(?:(?:www\\.)?)?(hastebin\\.com|hasteb\\.in|paste\\.helpch\\.at|haste\\.jkoenig\\.dev|haste\\.devcord\\.xyz|haste\\.schlaubi\\.me)\\/(?:raw\\/)?(.+?(?=\\.|\$|\\/|#))".toRegex()
+
+        // https://regex101.com/r/N8NBDz/2
+        private val PASTEBIN_PATTERN =
+            "(?:https?:\\/\\/(?:www\\.)?)?pastebin\\.com\\/(?:raw\\/)?(.+?(?=\\.|\$|\\/|#))".toRegex()
+
+        // https://regex101.com/r/CyjiKt/2
+        private val GHOSTBIN_PATTERN =
+            "(?:https?:\\/\\/(?:www\\.)?)?(?:ghostbin\\.co)\\/(?:paste\\/)?(.+?(?=\\.|\$|\\/))(?:\\/raw)?".toRegex()
+
+        // https://regex101.com/r/AlVYjn/2
+        private val GITHUB_GIST_PATTERN =
+            "(?:https?:\\/\\/)?(gist\\.github\\.com|gist.githubusercontent.com)\\/(.+?(?=\\.|\$|\\/))\\/(.+?(?=\\.|\$|\\/|#))".toRegex()
+
     }
 }
---
+++ src/main/kotlin/com/github/devcordde/devcordbot/util/Googler.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Daniel Scherf & Michael Rittmeister & Julian König
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.github.devcordde.devcordbot.util
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.customsearch.Customsearch
+import com.google.api.services.customsearch.model.Result
+
+/**
+ * Utility to google things.
+ */
+class Googler(private val apiKey: String, private val engineId: String) {
+
+    private val search =
+        Customsearch.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory(), null)
+            .setApplicationName("DevcordBot")
+            .build()
+
+    /**
+     * Googles the [query].
+     */
+    fun google(query: String): List<Result> {
+        return with(search.cse().list(query)) {
+            key = apiKey
+            cx = engineId
+            execute()
+        }.items ?: emptyList()
+    }
+}
