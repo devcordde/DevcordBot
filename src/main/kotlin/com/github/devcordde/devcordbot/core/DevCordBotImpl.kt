0ed6d4d2be06311e462d129a3b838e64e5416c42
Michael Rittmeister <michael@rittmeister.in>
2020-08-28 21:14:51 +0200

    fix tests (#124)
    * bugfix: Autohelp
    - Ignore whitespaces in exception names
    - Allow modifiers behind class name (extends, implements) (#113)
    - Fix bug in class name parsing
    - Ignore if caused by is prefix by something like `Caused by in exception
    - Update to Kotlin 1.4
    - Update to Gradle 6.6
    - Make tag names case insensitive (#105)
    - Remove un-permitted reactions in rat-chat (#122)
    - Fix to long raw tags causing exception (#119)
    
    * Implement requested changes
    
    * Fix syntax errors

--- src/main/kotlin/com/github/devcordde/devcordbot/core/DevCordBotImpl.kt
+++ src/main/kotlin/com/github/devcordde/devcordbot/core/DevCordBotImpl.kt
@@ -98,7 +98,7 @@ internal class DevCordBotImpl(
         .setStatus(OnlineStatus.DO_NOT_DISTURB)
         .setHttpClient(httpClient)
         .addEventListeners(
-            RatProtector(env["RAT_CHANNEL_ID"]!!.toLong(), env["RAT_ROLE_ID"]!!.toLong()),
+            RatProtector(env["RAT_CHANNEL_ID"]!!.toLong(), env["RAT_ROLE_ID"]!!.toLong(), this),
             MessageListener(),
             this@DevCordBotImpl,
             SelfMentionListener(this),
