40019441598983c418883585691067d5eb09f213
T1Il <till.schnegelsberg@gmail.com>
2021-01-28 22:18:17 +0100

    refactor: cleanup code
    Definitely selfmade. The goblin can't do anything

--- src/main/kotlin/com/github/devcordde/devcordbot/listeners/DevmarktRequestUpdater.kt
+++ src/main/kotlin/com/github/devcordde/devcordbot/listeners/DevmarktRequestUpdater.kt
@@ -1,67 +1,87 @@
+/*
+ * Copyright 2020 Daniel Scherf & Michael Rittmeister & Julian König
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.github.devcordde.devcordbot.listeners
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
 import net.dv8tion.jda.api.hooks.SubscribeEvent
 import okhttp3.FormBody
-import okhttp3.OkHttpClient
 import okhttp3.Request
 
-class DevmarktRequestUpdater(val requestChannel: String,val accessToken : String,val baseUrl : String,val emoteCheckId : String,val emoteBlockId : String) {
+/**
+ * Sends an event to the Devmarkt bot on emote action.
+ */
+class DevmarktRequestUpdater(
+    private val modChannel: String,
+    private val accessToken: String,
+    private val baseUrl: String,
+    private val emoteCheckId: String,
+    @Suppress("unused") private val emoteBlockId: String
+) {
 
+    private fun isNewEntryMessage(message: Message): Boolean {
+        val embeds = message.embeds
+        return (embeds.isNotEmpty() && "Neue Devmarkt-Anfrage" == embeds[0].title)
+    }
+
+    /**
+     * Reacts if a message is received
+     */
     @SubscribeEvent
-    fun onMessageReceived(event: MessageReceivedEvent) {
-        val channel = event.channel.id
-        if(channel != requestChannel) {
+    fun onMessageReceived(event: GuildMessageReceivedEvent) {
+        if (event.channel.id != modChannel
+            || event.message.member?.idLong != event.jda.selfUser.idLong
+            || !isNewEntryMessage(event.message)
+        ) {
             return
         }
-        val id = event.member?.id ?: return
-        val botid = event.jda.selfUser.id
-        if(!id.contentEquals(botid)) {
-            return
-        }
-        if(event.message.embeds.size == 0){
-            return
-        }
-        val title = event.message.embeds[0].title ?: return
-        val check = event.guild.getEmoteById(emoteCheckId) ?: return
 
-        if (!title.contentEquals("Neue Devmarkt-Anfrage")) {
-            return
-        }
-        print("breqq")
+        val check = event.guild.getEmoteById(emoteCheckId) ?: return
         event.message.addReaction(check).queue()
     }
 
+    /**
+     * Sends the event on an incoming reaction.
+     */
     @SubscribeEvent
     fun onReactionInDevmarktRequestChannel(event: MessageReactionAddEvent) {
-        val channel = event.channel.id
-        if (channel != requestChannel) {
+        if (event.channel.id != modChannel
+            || event.userId == event.jda.selfUser.id
+            || event.reactionEmote.id != emoteCheckId
+        ) {
             return
         }
-        val id = event.userId
-        val botId = event.jda.selfUser.id
-        if (id == botId) {
+
+        val message = event.retrieveMessage().complete()
+        if (!isNewEntryMessage(message)) {
             return
         }
-        val embed = event.retrieveMessage().complete().embeds[0] ?: return
-        val title = embed.title ?: return
-        if (!title.contentEquals("Neue Devmarkt-Anfrage")) {
-            return
-        }
-        if (event.reactionEmote.id == emoteCheckId) {
-            return
-        }
-        val requestId = embed.fields.stream()
+
+        val requestId = message.embeds[0].fields.stream()
             .filter { field -> "Request-ID" == field.name }
             .findAny()
-            .orElse(null).value
+            .orElse(null).value ?: return
 
         val formBody = FormBody.Builder()
-            .add("moderator_id", id)
+            .add("moderator_id", event.userId)
             .add("action", "accept")
             .add("access_token", accessToken)
-            .add("req_id", requestId!!)
+            .add("req_id", requestId)
             .build()
 
         val request = Request.Builder()
@@ -70,7 +90,5 @@ class DevmarktRequestUpdater(val requestChannel: String,val accessToken : String
             .build()
 
         event.jda.httpClient.newCall(request).execute()
-
     }
 }
-
