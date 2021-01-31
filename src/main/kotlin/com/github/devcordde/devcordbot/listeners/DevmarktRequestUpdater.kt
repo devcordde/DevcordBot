84b473b446d0c475ad411aedf7a96ab6aec15b9e
Till <46319158+T1Il@users.noreply.github.com>
2021-01-31 01:46:19 +0100

    Feature/devmarkt decline (#144)
    * feat: devmarkt decline wip
    
    * feat: accept and decline reason
    
    * feat: decline via reply
    
    * fix: newest jda version
    
    * refactor: cleanup code

--- src/main/kotlin/com/github/devcordde/devcordbot/listeners/DevmarktRequestUpdater.kt
+++ src/main/kotlin/com/github/devcordde/devcordbot/listeners/DevmarktRequestUpdater.kt
@@ -16,12 +16,14 @@
 
 package com.github.devcordde.devcordbot.listeners
 
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent
 import net.dv8tion.jda.api.hooks.SubscribeEvent
 import okhttp3.FormBody
 import okhttp3.Request
+import java.time.OffsetDateTime
 
 /**
  * Sends an event to the Devmarkt bot on emote action.
@@ -39,8 +41,20 @@ class DevmarktRequestUpdater(
         return (embeds.isNotEmpty() && "Neue Devmarkt-Anfrage" == embeds[0].title)
     }
 
+    private fun isNewReasonMessage(message: Message): Boolean {
+        val embeds = message.embeds
+        return (embeds.isNotEmpty() && "Begründung" == embeds[0].title)
+    }
+
+    private fun getFieldValue(message: Message, name: String): String? {
+        return message.embeds[0].fields.stream()
+            .filter { field -> name == field.name }
+            .findAny()
+            .orElse(null).value
+    }
+
     /**
-     * Reacts if a message is received
+     * Sends the event on an incoming reaction.
      */
     @SubscribeEvent
     fun onMessageReceived(event: GuildMessageReceivedEvent) {
@@ -72,10 +86,7 @@ class DevmarktRequestUpdater(
             return
         }
 
-        val requestId = message.embeds[0].fields.stream()
-            .filter { field -> "Request-ID" == field.name }
-            .findAny()
-            .orElse(null).value ?: return
+        val requestId = getFieldValue(message, "Request-ID") ?: return
 
         val formBody = FormBody.Builder()
             .add("moderator_id", event.userId)
@@ -90,5 +101,96 @@ class DevmarktRequestUpdater(
             .build()
 
         event.jda.httpClient.newCall(request).execute()
+    }
+
+    /**
+     * Reacts if a message is received
+     */
+    @SubscribeEvent
+    fun onReceiveDenyMessage(event: GuildMessageReceivedEvent) {
+        if (event.channel.id != modChannel) {
+            return
+        }
+
+        val check = event.guild.getEmoteById(emoteCheckId) ?: return
+        val block = event.guild.getEmoteById(emoteBlockId) ?: return
+        val message = event.message
+        val referencedMessage = event.message.referencedMessage ?: return
+
+        if (!isNewEntryMessage(referencedMessage) || !message.contentRaw.startsWith("deny:")) {
+            return
+        }
+
+        val reason = event.message.contentRaw
+        val builder = EmbedBuilder()
+        val user = event.member?.user ?: return
+
+        val requestId = getFieldValue(referencedMessage, "Request-ID") ?: return
+        val requestTitel = getFieldValue(referencedMessage, "Titel") ?: return
+        val requestAuthor = referencedMessage.embeds[0].footer?.text ?: return
+        val requestColor = referencedMessage.embeds[0].color ?: return
+
+        builder.setTitle("Begründung")
+        builder
+            .addField("Titel", "`$requestTitel`", true)
+            .addField("Author", "`$requestAuthor`", true)
+            .addField("Begründung", "`" + reason.replace("deny:", "") + "`", false)
+            .addField("Request-ID", requestId, true)
+            .setFooter(user.name + "#" + user.discriminator, user.effectiveAvatarUrl)
+            .setColor(requestColor)
+            .setTimestamp(OffsetDateTime.now())
+
+        val messageReason = event.channel.sendMessage(builder.build()).complete()
+        messageReason.addReaction(check).queue()
+        messageReason.addReaction(block).queue()
+        event.message.delete().queue()
+
+    }
+
+    /**
+     * Sends the event on an incoming reaction.
+     */
+    @SubscribeEvent
+    fun onReactonOnReasonMessage(event: MessageReactionAddEvent) {
+        if (event.channel.id != modChannel
+            || event.userId == event.jda.selfUser.id
+        ) {
+            return
+        }
+
+        val message = event.retrieveMessage().complete() ?: return
+
+        if (!isNewReasonMessage(message)) {
+            return
+        }
+
+        val reactionEmoteId = event.reactionEmote.id
+        val requestId = getFieldValue(message, "Request-ID") ?: return
+
+        if (reactionEmoteId == emoteBlockId) {
+            message.delete().queue()
+        }
+        if (reactionEmoteId != emoteCheckId) {
+            return
+        }
+
+        val reason = getFieldValue(message, "Begründung") ?: return
+
+        val formBody = FormBody.Builder()
+            .add("moderator_id", event.userId)
+            .add("action", "decline")
+            .add("access_token", accessToken)
+            .add("req_id", requestId)
+            .add("reason", reason)
+            .build()
+
+        val request = Request.Builder()
+            .url("$baseUrl/process.php")
+            .post(formBody)
+            .build()
+
+        message.delete().queue()
+        event.jda.httpClient.newCall(request).execute()
+
     }
 }
