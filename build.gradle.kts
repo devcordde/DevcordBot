3c58665717c80f529b44633cf8018106414c4460
Michael Rittmeister <michael@rittmeister.in>
2021-05-24 01:17:15 +0200

    fix: kord issues (#202)

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,71 +57,67 @@ dependencies {
     implementation("com.zaxxer", "HikariCP", "4.0.3")
 
     // Discord
-    implementation("dev.kord", "kord-core", "kotlin-1.5-20210514.081043-5") {
-        version {
-            strictly("kotlin-1.5-20210514.081043-5")
+    implementation("dev.kord", "kord-core", "kotlin-1.5-SNAPSHOT") {
+        implementation("dev.kord", "kord-core", "kotlin-1.5-20210514.081043-5") {
+            version {
+                strictly("kotlin-1.5-SNAPSHOT")
+            }
+        }
+
+        // Util
+        implementation("io.github.cdimascio", "java-dotenv", "5.2.2")
+        implementation("org.jetbrains.kotlinx", "kotlinx-cli", "0.2.1")
+        implementation("com.google.apis", "google-api-services-customsearch", "v1-rev20200917-1.31.0")
+        implementation("net.sf.trove4j", "trove4j", "3.0.3")
+
+        // Http
+        implementation(platform("io.ktor:ktor-bom:1.5.3"))
+        implementation("io.ktor", "ktor-client")
+        implementation("io.ktor", "ktor-client-okhttp")
+        implementation("io.ktor", "ktor-client-serialization-jvm")
+        implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.2.1")
+
+        // Config
+        implementation("com.github.uchuhimo.konf", "konf", "master-SNAPSHOT")
+        implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", "2.12.+")
+
+        // Autohelp
+        implementation("me.schlaubi.autohelp", "kord", "1.2.0-hotifx.1")
+        implementation("dev.schlaubi.forp", "forp-analyze-client", "1.0-SNAPSHOT")
+        implementation("com.vladsch.flexmark", "flexmark-html2md-converter", "0.60.2")
+
+        // Testing
+        testImplementation("org.mockito", "mockito-core", "3.8.0")
+        testImplementation("com.nhaarman.mockitokotlin2", "mockito-kotlin", "2.2.0")
+        testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.7.1")
+        testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.7.1")
+    }
+
+    application {
+        mainClass.set("com.github.devcordde.devcordbot.LauncherKt")
+    }
+
+    tasks {
+        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "16"
+                freeCompilerArgs =
+                    freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn" + "-Xopt-in=dev.kord.common.annotation.KordPreview" + "-Xopt-in=dev.kord.common.annotation.KordExperimental"
+            }
+        }
+
+        test {
+            useJUnitPlatform()
+        }
+
+        installDist {
+            destinationDir = buildDir.resolve("libs/install")
         }
     }
-
-    // Util
-    implementation("io.github.cdimascio", "java-dotenv", "5.2.2")
-    implementation("org.jetbrains.kotlinx", "kotlinx-cli", "0.2.1")
-    implementation("com.google.apis", "google-api-services-customsearch", "v1-rev20200917-1.31.0")
-    implementation("net.sf.trove4j", "trove4j", "3.0.3")
-
-    // Http
-    implementation(platform("io.ktor:ktor-bom:1.5.3"))
-    implementation("io.ktor", "ktor-client")
-    implementation("io.ktor", "ktor-client-okhttp")
-    implementation("io.ktor", "ktor-client-serialization-jvm") {
-        exclude("org.jetbrains.kotlinx", "kotlinx-serialization-json")
-    }
-    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.0.0") {
-        version {
-            strictly("1.0.0")
-        }
-    }
-
-    // Config
-    implementation("com.github.uchuhimo.konf", "konf", "master-SNAPSHOT")
-    implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", "2.12.+")
-
-    // Autohelp
-    implementation("me.schlaubi.autohelp", "kord", "1.2.0-hotifx.1")
-    implementation("dev.schlaubi.forp", "forp-analyze-client", "1.0-SNAPSHOT")
-    implementation("com.vladsch.flexmark", "flexmark-html2md-converter", "0.60.2")
-
-    // Testing
-    testImplementation("org.mockito", "mockito-core", "3.8.0")
-    testImplementation("com.nhaarman.mockitokotlin2", "mockito-kotlin", "2.2.0")
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.7.1")
-    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.7.1")
 }
 
-application {
-    mainClass.set("com.github.devcordde.devcordbot.LauncherKt")
-}
-
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "16"
-            freeCompilerArgs =
-                freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn" + "-Xopt-in=dev.kord.common.annotation.KordPreview" + "-Xopt-in=dev.kord.common.annotation.KordExperimental"
-        }
-    }
-
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        disabledRules.set(setOf("no-wildcard-imports"))
-    }
-
-    test {
-        useJUnitPlatform()
-    }
-
-    installDist {
-        destinationDir = buildDir.resolve("libs/install")
-    }
+configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+    disabledRules.set(setOf("no-wildcard-imports"))
 }
 
 /**
