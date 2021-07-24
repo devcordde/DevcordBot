0581e9f0456c156f1440e16e6ba6ee730b120078
Niklas <51596575+base2code@users.noreply.github.com>
2021-07-24 23:06:41 +0200

    Update TagCommand.kt

--- src/main/kotlin/com/github/devcordde/devcordbot/commands/general/TagCommand.kt
+++ src/main/kotlin/com/github/devcordde/devcordbot/commands/general/TagCommand.kt
@@ -106,8 +106,8 @@ class TagCommand : AbstractRootCommand() {
             if (newSuspendedTransaction { checkTagExists(name, context) }) return
             val status = context.respond(
                 Embeds.info(
-                    "Bitte gebe den Inhalt an!",
-                    "Bitte gebe den Inhalt des Tags in einer neuen Nachricht an."
+                    "Bitte gib den Inhalt an!",
+                    "Bitte gib den Inhalt des Tags in einer neuen Nachricht an."
                 )
             )
 
@@ -181,8 +181,8 @@ class TagCommand : AbstractRootCommand() {
 
             val status = context.respond(
                 Embeds.info(
-                    "Bitte gebe den Inhalt an!",
-                    "Bitte gebe den Inhalt des Tags in einer neuen Nachricht an."
+                    "Bitte gib den Inhalt an!",
+                    "Bitte gib den Inhalt des Tags in einer neuen Nachricht an."
                 )
             )
 
